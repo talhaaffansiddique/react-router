13267300bb2d0c73a8fc20dc16d3da06a2a69f09
Talha Affan Siddique <talhaaffansiddique@gmail.com>
2023-09-15 11:27:08 +0500

    167 - Error Page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
+import Error from "./pages/Error";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
